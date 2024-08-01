2cd7ddba3e77d56c4f17019a7d6653d179693493
Christ GNABO <christ-heaven@localhost.localdomain>
2024-08-01 03:15:03 +0000

    media files

---
+++ lib/screens/onboding/onbodingpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnbodingPageScreen extends StatefulWidget {
+  const OnbodingPageScreen({super.key});
+
+  @override
+  State<OnbodingPageScreen> createState() => _OnbodingPageScreenState();
+}
+
+class _OnbodingPageScreenState extends State<OnbodingPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
