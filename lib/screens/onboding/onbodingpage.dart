2982e8afa594c71fd89a276b746fc4124e271a45
Christ GNABO <christ-heaven@192.168.1.14>
2024-08-03 20:16:43 +0000

    seond files

--- lib/screens/onboding/onbodingpage.dart
+++ lib/screens/onboding/onbodingpage.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:oranga_sante1/screens/home/homepage.dart';
 
 class OnbodingPageScreen extends StatefulWidget {
   const OnbodingPageScreen({super.key});
@@ -9,7 +13,35 @@ class OnbodingPageScreen extends StatefulWidget {
 
 class _OnbodingPageScreenState extends State<OnbodingPageScreen> {
   @override
+  void initState() {
+    super.initState();
+    // this is the shift who will run the homepagescreen after
+    Timer(
+        const Duration(seconds: 20),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const Homepagescreen())));
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFF273e32),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Lottie.asset(
+                'assets/files/Animation1.json',
+                fit: BoxFit.contain,
+                width: 500,
+                height: 500,
+                repeat: true,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
