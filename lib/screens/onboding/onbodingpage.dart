ac81f3c108464e647c9cd7a4b3e6bdf4d497b9c2
Christ GNABO <christ-heaven@192.168.1.14>
2024-08-06 00:12:12 +0000

    three day

--- lib/screens/onboding/onbodingpage.dart
+++ lib/screens/onboding/onbodingpage.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:oranga_sante1/screens/home/homepage.dart';
 
@@ -12,12 +13,17 @@ class OnboardingPageScreen extends StatefulWidget {
 }
 
 class _OnboardingPageScreenState extends State<OnboardingPageScreen> {
+  bool _showText = false;
+
   @override
   void initState() {
     super.initState();
-    // Cette partie exécutera la navigation vers la page d'accueil après 20 secondes
+
+    // Activer le mode plein écran
+    _enableFullScreenMode();
+
     Timer(
-      const Duration(seconds: 5),
+      const Duration(seconds: 10),
       () {
         if (mounted) {
           Navigator.pushReplacement(
@@ -27,25 +33,86 @@ class _OnboardingPageScreenState extends State<OnboardingPageScreen> {
         }
       },
     );
+    Timer(
+      const Duration(seconds: 1),
+      () {
+        if (mounted) {
+          setState(() {
+            _showText = true;
+          });
+        }
+      },
+    );
+  }
+
+  void _enableFullScreenMode() {
+    // Masquer les barres de statut et de navigation pour le mode plein écran
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge, overlays: []);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF273e32),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.network(
-              "https://lottie.host/b4fc70ec-c1ed-4de0-98b7-c76e8df902c9/jwW87NPz7X.json",
-              fit: BoxFit.contain,
-              width: 150,
-              height: 150,
-              repeat: true,
+      backgroundColor: const Color(0xFF000000),
+      body: Stack(
+        children: [
+          // Background image
+          Align(
+            alignment: Alignment
+                .centerRight, // Aligner l'image au centre depuis la droite
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                      'assets/images/img4.jpg'), // Chemin vers votre image
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          // Foreground content
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.network(
+                  "https://lottie.host/b4fc70ec-c1ed-4de0-98b7-c76e8df902c9/jwW87NPz7X.json",
+                  fit: BoxFit.contain,
+                  width: 100,
+                  height: 100,
+                  repeat: true,
+                ),
+                const SizedBox(height: 10),
+                Visibility(
+                  visible: _showText,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 15.0,
+                      color: Colors
+                          .white, // Assurer que le texte est visible sur l'image de fond
+                    ),
+                    child: AnimatedTextKit(
+                      animatedTexts: [
+                        WavyAnimatedText(
+                          'Votre santé à proximité.',
+                          speed: const Duration(milliseconds: 150),
+                        ),
+                        WavyAnimatedText(
+                          'Your health close at hand.',
+                          speed: const Duration(milliseconds: 150),
+                        ),
+                      ],
+                      isRepeatingAnimation: true,
+                      onTap: () {
+                        print("Tap Event");
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
